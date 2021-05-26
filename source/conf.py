3708e3876d019bf1d4efd86e0651987168d627fa
joseph-torsney <joseph.torsney@stfc.ac.uk>
2021-05-26 13:52:09 +0100

    Setup custom index page

--- source/conf.py
+++ source/conf.py
@@ -91,6 +91,7 @@ html_static_path = ["_static"]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
+# html_additional_pages = {'index': 'index.html'}
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
